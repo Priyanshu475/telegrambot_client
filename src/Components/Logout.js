857d4ec9b81395a8ff03fc1e612c73f955f9d9ed
Priyanshu Dubey <96469123+Priyanshu475@users.noreply.github.com>
2023-12-19 22:36:59 +0530

    Update Logout.js

--- src/Components/Logout.js
+++ src/Components/Logout.js
@@ -1,35 +1,35 @@
 import React from "react";
 import { googleLogout } from "@react-oauth/google";
-import { useNavigate } from "react-router-dom"; // Import useNavigate
+import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
+import { styled } from "@mui/system";
 
+const StyledButton = styled(Button)({
+  marginLeft: "20px",
+  borderRadius: "8px",
+  fontWeight: "bold",
+  boxShadow: "0 3px 5px 2px rgba(255, 105, 135, 0.3)",
+  backgroundColor: "#f44336", // Red color
+  color: "#fff", // White text
+  "&:hover": {
+    backgroundColor: "#d32f2f", // Darker red on hover
+  },
+});
 
 const LogoutButton = () => {
-  const navigate = useNavigate(); // Initialize navigate
+  const navigate = useNavigate();
+
   const handleLogout = () => {
     googleLogout();
     console.log("pressed");
     localStorage.removeItem("user");
-    navigate("/"); // Use navigate to go to the root path
+    navigate("/");
   };
 
   return (
-    <>
-      <div>
-        <Button
-          // variant="contained"
-          color="error"
-          onClick={handleLogout}
-          style={{
-            borderRadius: "50px", // Makes the button more rounded
-            fontWeight: "bold", // Makes the text bold
-            boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)", // Adds a subtle shadow
-          }}
-        >
-          Logout
-        </Button>
-      </div>
-    </>
+    <div>
+      <StyledButton onClick={handleLogout}>Logout</StyledButton>
+    </div>
   );
 };
 
